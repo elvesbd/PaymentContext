8100eead73ff6769ee23429fb2a250fedd2236c1
elvesbd <elvesbd@yahoo.com>
2023-07-24 10:55:10 -0300

    test: ensure return an null when document not exists

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,33 @@
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Queries;
+
+[TestClass]
+public class StudentQueriesTests
+{
+    private IList<Student>? _students;
+    public StudentQueriesTests()
+    {
+        _students = new List<Student>();
+        for (var i = 0; i <= 10; i++)
+        {
+            _students.Add(new Student(
+                   new Name("Aluno", i.ToString()),
+                   new Document("1111111111" + i.ToString(), EDocumentType.CPF),
+                   new Email(i.ToString() + "@balta.io")
+               ));
+        }
+    }
+
+    [TestMethod]
+    public void ShouldReturnNullWhenDocumentNotExists()
+    {
+        var exp = StudentQueries.GetStudentInfo("12345678911");
+        var student = _students?.AsQueryable().Where(exp).FirstOrDefault();
+
+        Assert.AreEqual(null, student);
+    }
+}
